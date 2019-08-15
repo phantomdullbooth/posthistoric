444eda01f763ec260c6fca45ae7cf862d725315f
Aegean <aegeanthompson@gmail.com>
2019-08-15 17:17:19 -0500

    updating code for heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+const PROJECT3_DB = process.env.PROJECT3_DB;
+
+// Connect to Mongo
+mongoose.connect(PROJECT3_DB ,  { useNewUrlParser: true});
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', PROJECT3_DB));
+db.on('disconnected', () => console.log('mongo disconnected'));
