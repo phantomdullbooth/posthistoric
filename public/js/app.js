92f13565049a31a55a87d5592736720c46346349
alyssadm <ahlisa.dm@gmail.com>
2019-08-16 10:37:20 -0400

    added parent and auth controllers, organized app.js into sections

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,55 @@
 const app = angular.module('App', [])
 
+// =====================================================================
+//                          PARENT CONTROLLER
+// =====================================================================
+// * ATTACHED TO: <body> tag
+// * Contains the properties for the $rootScope (see google doc for details)
+app.controller('ParentController', ['$http', '$rootScope', function($http, $rootScope){
+
+  // ==============
+  // INITIAL VALUES
+  // ==============
+  $rootScope.currentUser = null;
+
+}]);
+
+// =====================================================================
+//                            MAIN CONTROLLER
+// =====================================================================
+// * ATTACHED TO: <div class="container"> tag
+// * Contains the main functionality for the app
 app.controller('Controller', function(){
     this.goo = 'ber';
 })
+
+// =====================================================================
+//                        AUTHORIZATION CONTROLLER
+// =====================================================================
+// * ATTACHED TO: <header> tag
+// * Contains the functionality for logging in/logging out/signing up
+app.controller('AuthController', ['$http', '$rootScope', function($http, $rootScope){
+
+  // ==============
+  // INITIAL VALUES
+  // ==============
+  const controller = this;
+  this.username = null;
+  this.password = null;
+  this.newUsername = null;
+  this.newPassword = null;
+
+  // ==============
+  // RESTFUL ROUTES
+  // ==============
+
+  // CREATE NEW USER (POST)
+
+  // CREATE NEW SESSION (POST)
+
+  // DESTROY SESSION (DELETE)
+
+  // GET SESSION
+
+
+}]);
