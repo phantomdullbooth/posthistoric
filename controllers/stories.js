4a076c1ad4a3ed7b5dbb94fba49209437f976040
Aegean <aegeanthompson@gmail.com>
2019-08-16 10:45:27 -0500

    added comments to controllers/stories.js

--- controllers/stories.js
+++ controllers/stories.js
@@ -4,24 +4,28 @@ const User = require('../models/users.js');
 const Stories = require('../models/stories.js');
 const bcrypt = require('bcrypt');
 
+/// FIND ALL STORIES ///
 router.get('/', (req, res) => {
 Stories.find({}, (err, foundStories) => {
   res.json(foundStories);
   });
 });
 
+/// DELETE A STORY ///
 router.delete('/:id', (req, res) => {
   Stories.findByIdAndRemove(req.params.id, (err, deletedStory) => {
     res.json(deletedStory);
   });
 });
 
+/// EDIT A STORY ///
 router.put('/:id', (req, res) => {
   Stories.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedStory) => {
     res.json(updatedStory);
   });
 });
 
+/// CREATE NEW STORY ///
 router.post('/', (req, res) => {
   Stories.create(req.body, (err, createdStory) => {
     res.json(createdStory);
